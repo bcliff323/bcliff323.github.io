af8133ad802905341da20cdeb069ae58932abbeb
Brian Clifford <1113621+bcliff323@users.noreply.github.com>
2022-11-09 15:56:39 -0500

    Componentize all sections

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,3 +5,23 @@ module.exports = function (eleventyConfig) {
 		components: "_includes/**/*.webc"
 	});
 };
+
+const Image = require("@11ty/eleventy-img");
+//dist/images/recipe-as-a-service.png
+//dist/images/rei-siv.jpg
+//dist/images/vacation-vermont.png
+
+(async () => {
+	const urls = [
+		"https://brianclifford.com/dist/images/recipe-as-a-service.png",
+		"https://brianclifford.com/dist/images/rei-siv.jpg",
+		"https://brianclifford.com/dist/images/vacation-vermont.png"
+	];
+	const stats = await Promise.all(urls.map(url => Image(url, {
+		width: [300],
+		outputDir: "./_site/img/",
+		cacheOptions: {
+			directory: "./_site/.cache"
+		}
+	})));
+})();
